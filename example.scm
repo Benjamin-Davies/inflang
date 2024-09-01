b4164b19df5168ba8cd6b661ab85e27cafc3d67a
Benjamin Davies <bentendavies@gmail.com>
2024-09-01 14:11:55 +1200

    Add files

---
+++ example.scm
@@ -0,0 +1,20 @@
+(add-to-load-path ".")
+(use-modules (inflang prelude))
+
+(define (fazz-buzz en)
+    (cond
+        ((aq (mad en fefteen) zero)
+            (say "fazz-buzz"))
+        ((aq (mad en three) zero)
+            (say "fazz"))
+        ((aq (mad en feve) zero)
+            (say "buzz"))
+        (else
+            (say en))))
+
+(define (main)
+    (fur (range-inc wen twenty)
+        (lambda (en) (fazz-buzz en))))
+
+(main)
+(range)
